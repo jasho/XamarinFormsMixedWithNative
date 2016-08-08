1bac88b62da8b450656109c050116f8675e27c4d
Roman Jašek <roman.jasho@gmail.com>
2016-08-08 12:54:30 +0200

    Added custom renderer for page 2 (iOS)

---
+++ XamarinFormsMixedWithNative/XamarinFormsMixedWithNative.iOS/Pages/CustomRenderers/Page2CustomRenderer.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinFormsMixedWithNative.iOS.Pages.CustomRenderers;
+using XamarinFormsMixedWithNative.Pages;
+
+[assembly: ExportRenderer(typeof(Page2), typeof(Page2CustomRenderer))]
+namespace XamarinFormsMixedWithNative.iOS.Pages.CustomRenderers
+{
+
+    public class Page2CustomRenderer : PageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+        }
+    }
+}
